4f7cf8f9b9e5624879695ad563baf7fd8e0adf9d
sonukushwaha55 <sonukumar1772001@gmail.com>
2023-10-26 10:38:10 +0530

    changes

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("Please Clicked on the button")
+
+var bulb = document.querySelector(".bulb")
+
+var flag = 0
+
+var button = document.querySelector("button")
+button.addEventListener("click", function(){
+if (flag == 0) {
+    bulb.style.backgroundColor = "yellow"
+    console.log("clicked");
+    flag = 1
+}else{
+    bulb.style.backgroundColor = "transparent"
+    flag = 0
+    console.log("Again Clicked");
+}
+})
